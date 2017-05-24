b4b216c110ffd7816ebd8ed953516a8f133b1393
timothy mitchell <timothy.p.mitchell92@gmail.com>
2017-05-24 16:04:16 -0700

    used ajax

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 'use strict';
-
+// const requestProxy = require('express-request-proxy');
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 5000;
 
@@ -15,3 +15,17 @@ app.get('/index.html', function(request, response){
 app.listen(PORT, function() {
   console.log('The port we are using is ' + PORT)
 });
+
+app.get('*', function(request, response){
+  console.log('Index HTML');
+  response.sendFile('index.html', { root: './public'});
+})
+
+function proxyGitHub(request, response) {
+  (requestProxy({
+    url: `https://api.github.com/${request.params[0]}`,
+    headers: {Authorization: `token ${process.env.GITHUB_TOKEN}`}
+  }))(request, response);
+}
+
+app.get('/github/*', proxyGitHub);
